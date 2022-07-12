5329d5622087065e3f8865e35077ea527a7efaf2
Evans Djangbah <djangbahevans@yahoo.com>
2022-07-12 23:56:58 +0000

    Set up backend to handle both http and ws comment posts

--- v1/index.ts
+++ v1/index.ts
@@ -2,6 +2,14 @@ import express from 'express';
 import { createServer } from 'http';
 import { Server } from "socket.io";
 
+type CommentType = {
+  author: string,
+  text: string,
+  image: string,
+}
+
+const COMMENTS: (CommentType & { id: number, timestamp: number })[] = []
+
 const app = express();
 const server = createServer(app);
 const io = new Server(server);
@@ -15,11 +23,38 @@ io.on('connection', (socket) => {
   socket.on('disconnect', () => {
     console.log('user disconnected');
   });
-  socket.on('chat message', (msg) => {
-    console.log('message: ' + msg);
-    io.emit('chat message', msg);
+  socket.on('comment', (msg: CommentType) => {
+    COMMENTS.push({
+      ...msg,
+      id: COMMENTS.length,
+      timestamp: Date.now(),
+    });
+    io.emit('comment', msg);
   });
 });
+
+app.get("/comments", (req, res) => {
+  res.send(COMMENTS);
+})
+
+app.post("/comments", (req, res) => {
+  const { author, text, image } = req.body;
+  COMMENTS.push({
+    author,
+    text,
+    image,
+    id: COMMENTS.length,
+    timestamp: Date.now(),
+  });
+  io.emit('comment', {
+    author,
+    text,
+    image,
+    id: COMMENTS.length,
+    timestamp: Date.now(),
+  });
+  res.send(COMMENTS);
+})
 
 server.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
